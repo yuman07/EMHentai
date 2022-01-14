1083f6bfebc70036a7b08e3de9721a80ed18c083
yuman <yuman@microsoft.com>
2022-01-14 10:30:38 +0800

    Initial Commit

---
+++ EMHenTai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMHenTai
+//
+//  Created by yuman on 2022/1/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
