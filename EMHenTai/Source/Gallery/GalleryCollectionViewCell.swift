381c93c1ae29cb4bf0f0f7ffd81db185f4a861bb
yuman <yuman@microsoft.com>
2022-01-21 17:36:32 +0800

    支持pinch

--- EMHenTai/Source/Gallery/GalleryCollectionViewCell.swift
+++ EMHenTai/Source/Gallery/GalleryCollectionViewCell.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class GalleryCollectionViewCell: UICollectionViewCell {
     
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.bounces = false
+        view.delegate = self
+        view.maximumZoomScale = 3
+        view.bouncesZoom = false
+        return view
+    }()
+    
     private let imageView: UIImageView = {
         let img = UIImageView()
         img.contentMode = .scaleAspectFit
@@ -28,18 +37,33 @@ class GalleryCollectionViewCell: UICollectionViewCell {
     private func setupUI() {
         self.backgroundColor = .black
         contentView.backgroundColor = .black
-        contentView.addSubview(imageView)
+        contentView.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        scrollView.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
-        imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: scrollView.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: scrollView.rightAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
     }
     
     func updateImageWith(filePath: String) {
         if let data = try? Data(contentsOf: URL(fileURLWithPath: filePath)) {
             imageView.image = UIImage(data: data)
         }
+    }
+}
+
+extension GalleryCollectionViewCell: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
