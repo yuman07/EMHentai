237ac81d06c405a3225a9b50c32d2aba9efb93df
yuman <yuman@microsoft.com>
2022-01-21 11:16:46 +0800

    设置页和登录

--- EMHenTai/Source/Login/LoginViewController.swift
+++ EMHenTai/Source/Login/LoginViewController.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import UIKit
+import WebKit
+
+class LoginViewController: UIViewController {
+    private static let loginURL = "https://forums.e-hentai.org/index.php?act=Login&CODE=01"
+    
+    private let webView = WKWebView()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        hidesBottomBarWhenPushed = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupData()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemGroupedBackground
+        view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        webView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        webView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    private func setupData() {
+        guard let url = URL(string: LoginViewController.loginURL) else { return }
+        webView.load(URLRequest(url: url))
+    }
+}
--- EMHenTai/Source/Setting/SettingViewController.swift
+++ EMHenTai/Source/Setting/SettingViewController.swift
@@ -10,7 +10,108 @@ import UIKit
 
 class SettingViewController: UIViewController {
     
+    private lazy var tableView: UITableView = {
+        let table = UITableView(frame: CGRect.zero, style: .grouped)
+        table.estimatedRowHeight = 0
+        table.estimatedSectionHeaderHeight = 0
+        table.estimatedSectionFooterHeight = 0
+        table.bounces = false
+        table.rowHeight = 44
+        table.delegate = self
+        table.dataSource = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: NSStringFromClass(UITableViewCell.self))
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemGroupedBackground
+        view.addSubview(tableView)
+        
+        navigationItem.title = "设置"
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+}
+
+extension SettingViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 2
+        case 2:
+            return 2
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        30
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "登录状态"
+        case 1:
+            return "语言(重启生效)"
+        case 2:
+            return "磁盘占用"
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(UITableViewCell.self), for: indexPath)
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = SettingManager.shared.isLogin ? "已登录(点击可登出)" : "未登录(点击去登录)"
+            return cell
+        case 1:
+            cell.textLabel?.text = indexPath.row == 0 ? "中文" : "英文"
+            return cell
+        case 2:
+            cell.textLabel?.text = indexPath.row == 0 ? "下载数据" : "历史数据"
+            return cell
+        default:
+            return cell
+        }
+    }
+}
+
+extension SettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath.section {
+        case 0:
+            if SettingManager.shared.isLogin {
+                tableView.reloadData()
+            } else {
+                navigationController?.pushViewController(LoginViewController(), animated: true)
+            }
+        case 1:
+            break
+        case 2:
+            break
+        default:
+            break
+        }
     }
 }
