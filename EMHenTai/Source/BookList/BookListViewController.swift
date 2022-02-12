3bc723caeab6741c1febf491d6e4d66beedb37fc
yuman <yuman@microsoft.com>
2022-02-12 19:29:48 +0800

    优化判断

--- EMHenTai/Source/BookList/BookListViewController.swift
+++ EMHenTai/Source/BookList/BookListViewController.swift
@@ -150,7 +150,7 @@ extension BookListViewController {
         case .Home:
             navigationController?.pushViewController(SearchViewController(), animated: true)
         case .History:
-            guard !DBManager.shared.booksMap[.history]!.isEmpty else { return }
+            guard !books.isEmpty else { return }
             let vc = UIAlertController(title: "提示", message: "确定要清除所有历史记录吗？\n(不会影响已下载内容)", preferredStyle: .alert)
             vc.addAction(UIAlertAction(title: "清除", style: .default, handler: { _ in
                 DBManager.shared.booksMap[.history]!
