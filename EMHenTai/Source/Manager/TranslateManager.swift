d5f12e0c7b509c55147c602ec5a17a2bfd804613
yuman <yuman@microsoft.com>
2022-05-28 15:47:02 +0800

    update kf && tag

--- EMHenTai/Source/Manager/TranslateManager.swift
+++ EMHenTai/Source/Manager/TranslateManager.swift
@@ -9,7 +9,7 @@ import Foundation
 
 final class TranslateManager {
     static let shared = TranslateManager()
-    private static let jsonName = "tag-v6.4235.1-220424.json"    // https://github.com/EhTagTranslation/Database/releases    db.text.json
+    private static let jsonName = "tag-v6.4485.1-220528.json"    // https://github.com/EhTagTranslation/Database/releases    db.text.json
     private init () {
         guard let url = Bundle.main.url(forResource: TranslateManager.jsonName, withExtension: nil),
               let data = try? Data(contentsOf: url),
