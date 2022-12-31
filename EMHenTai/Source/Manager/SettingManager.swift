8bd69c7469d934e81a0ebaa2ec08d0f682a278cc
yuman <yuman@microsoft.com>
2022-12-31 19:02:12 +0800

    update code

--- EMHenTai/Source/Manager/SettingManager.swift
+++ EMHenTai/Source/Manager/SettingManager.swift
@@ -77,6 +77,16 @@ final class SettingManager {
     func clearOtherData() async {
         await withUnsafeContinuation({ continuation in
             KingfisherManager.shared.cache.clearDiskCache {
+                guard let folders = try? FileManager.default.contentsOfDirectory(atPath: Book.downloadFolderPath), !folders.isEmpty else {
+                    continuation.resume()
+                    return
+                }
+                folders.compactMap({ Int($0) }).forEach {
+                    let path = Book.downloadFolderPath + "/\($0)"
+                    if !DBManager.shared.contains(gid: $0, of: .download) && !DBManager.shared.contains(gid: $0, of: .history) {
+                        try? FileManager.default.removeItem(atPath: path)
+                    }
+                }
                 continuation.resume()
             }
         })
