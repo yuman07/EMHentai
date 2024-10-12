723b16c358027be1e550a5056f2c31b0eea867d3
yuman <yuman@microsoft.com>
2024-10-12 16:47:23 +0800

    update code

--- EMHenTai/Source/Tools/AFSession+Tools.swift
+++ EMHenTai/Source/Tools/AFSession+Tools.swift
@@ -9,7 +9,7 @@ import Alamofire
 import Foundation
 
 let emSession: Session = {
-  let configuration = URLSessionConfiguration.af.default
-  configuration.timeoutIntervalForRequest = 10
-  return Session(configuration: configuration)
+    let configuration = URLSessionConfiguration.af.default
+    configuration.timeoutIntervalForRequest = 10
+    return Session(configuration: configuration)
 }()
